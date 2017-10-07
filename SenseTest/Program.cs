914d369692fb70df7e5584f92ad3022213a6d8d7
Johannes Egger <eggerjohansi@gmail.com>
2017-10-07 23:16:56 +0200

    Change led pixel test

--- SenseTest/Program.cs
+++ SenseTest/Program.cs
@@ -17,14 +17,10 @@ namespace SenseTest
         {
             var text = args.Length > 0 ? args[0] : "Hello World";
             var sense = Sense.Create();
-            var ledPixels = LedPixelsFromText.Create(text).SetColor(new Color(255, 255, 255));
+            var ledPixels = LedPixelsFromText
+                .Create(text)
+                .SetColor(new Color(128, 128, 128));
             var maxShift = -ledPixels.Pixels.Max(p => p.Cell.Column) - 1;
-            var pixels = ImmutableList<CellColor>
-                .Empty
-                .Add(new CellColor(new Cell(0, 0), new Color(255, 0, 0)))
-                .Add(new CellColor(new Cell(7, 7), new Color(0, 255, 0)));
-            sense.SetPixels(new LedPixels(pixels));
-            Thread.Sleep(1000);
             for (int i = 0; i >= maxShift; i--)
             {
                 sense.SetPixels(ledPixels.Shift(0, i));
