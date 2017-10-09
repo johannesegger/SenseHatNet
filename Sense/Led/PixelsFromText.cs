0b164dd7fb306eeccf2d95e198847df0319936b0
Johannes Egger <eggerjohansi@gmail.com>
2017-10-09 21:35:11 +0200

    Copy LED font to output directory

--- Sense/Led/PixelsFromText.cs
+++ Sense/Led/PixelsFromText.cs
@@ -17,7 +17,7 @@ namespace Sense.Led
             {
                 var fontCollection = new FontCollection();
                 var assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                var fontFamily = fontCollection.Install(Path.Combine(assemblyDir, @"SCUMM-8px-unicode.ttf"));
+                var fontFamily = fontCollection.Install(Path.Combine(assemblyDir, "Resources", @"SCUMM-8px-unicode.ttf"));
                 var font = fontFamily.CreateFont(8, FontStyle.Regular);
                 image.Mutate(ctx => ctx.DrawText(text, font, Rgba32.Black, PointF.Empty));
                 // using (var stream = System.IO.File.OpenWrite(@"output.png"))
