9d2f02540c47be7594e0d4c95faa4105fe91d684
Johannes Egger <eggerjohansi@gmail.com>
2017-10-29 17:44:40 +0100

    Remove accidentally added script

--- publish-and-deploy.ps1
+++
@@ -1,4 +0,0 @@
-Push-Location $PSScriptRoot
-dotnet publish -r linux-arm SenseTest
-SSHSync\ssh-sync\run.ps1 SenseTest\bin\Debug\netcoreapp2.0\linux-arm\publish pi 192.168.0.101 /home/pi/sense-test
-Pop-Location
