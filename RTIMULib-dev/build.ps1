a749181ac956b4b44d4a435a50c6c61c44572bd6
johannesegger <j.egger@posteo.at>
2020-06-06 13:55:50 +0200

    Fix RTIMULib build

--- RTIMULib-dev/build.ps1
+++ RTIMULib-dev/build.ps1
@@ -6,10 +6,11 @@ Write-Output "# Building docker image"
 docker build -f .\Dockerfile -t $imageName ..
 
 Write-Output "# Starting docker container to compile RTIMULibWrapper"
+$baseDir = Split-Path -Parent $pwd
 docker run `
     --rm `
-    -v "${pwd}\..\RTIMULibWrapper:/opt/RTIMULibWrapper" `
-    -v "${pwd}\..\Sense\Native:/opt/bin" `
+    -v "$baseDir\RTIMULibWrapper:/opt/RTIMULibWrapper" `
+    -v "$baseDir\Sense\Native:/opt/bin" `
     $imageName
 
 Pop-Location
